873b1a7579ffd67ef77a984803eb507699a493f4
Julien Fraisse <roulyo.42@gmail.com>
2012-03-30 11:44:24 +0200

    Created maven project.

---
+++ src/main/java/com/epita/mti/plic/opensource/controlibserversample/App.java
@@ -0,0 +1,13 @@
+package com.epita.mti.plic.opensource.controlibserversample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
