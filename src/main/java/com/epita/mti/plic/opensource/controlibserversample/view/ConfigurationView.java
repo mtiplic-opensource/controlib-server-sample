ae20679672645d1fc06f62d47822ddfe3e161dbf
Julien Fraisse <roulyo.42@gmail.com>
2012-06-05 16:22:23 +0200

    Added forgotten files.

---
+++ src/main/java/com/epita/mti/plic/opensource/controlibserversample/view/ConfigurationView.java
@@ -0,0 +1,79 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.epita.mti.plic.opensource.controlibserversample.view;
+
+import com.epita.mti.plic.opensource.controlibserversample.Server;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ *
+ * @author Julien "Roulyo" Fraisse
+ */
+public class ConfigurationView extends Frame
+{
+
+  private TextField csField = new TextField(Server.getServerConfiguration().getOutputPort().toString());
+  private TextField scField = new TextField(Server.getServerConfiguration().getInputPort().toString());
+  private Button cancelButton = new Button("Cancel");
+  private Button okButton = new Button("Ok");
+  private ConfigurationView view = null;
+
+  public ConfigurationView()
+  {
+    super("Settings");
+
+    Dimension dim = getToolkit().getScreenSize();
+
+    add(new Label("Server to Client port:"));
+    add(csField);
+    add(new Label("Client to Server port:"));
+    add(scField);
+    add(new Label("Values must be between 1000 and 65535."));
+    add(new Label("Changes will occur at restart."));
+    add(cancelButton);
+    add(okButton);
+
+    addWindowListener(new WindowAdapter()
+    {
+
+      @Override
+      public void windowClosing(WindowEvent we)
+      {
+        dispose();
+      }
+    });
+
+    cancelButton.addActionListener(new ActionListener()
+    {
+
+      @Override
+      public void actionPerformed(ActionEvent e)
+      {
+        dispose();
+      }
+    });
+    
+    okButton.addActionListener(new ActionListener()
+    {
+
+      @Override
+      public void actionPerformed(ActionEvent e)
+      {
+        Server.getServerConfiguration().setInputPort(Integer.parseInt(scField.getText()));
+        Server.getServerConfiguration().setOutputPort(Integer.parseInt(csField.getText()));
+        dispose();
+      }
+    });
+
+    setLayout(new GridLayout(4, 2));
+    setSize(520, 180);
+    setLocation((dim.width - getBounds().width) / 2, (dim.height - getBounds().height) / 2);
+    setVisible(true);
+  }
+}
