551ed52228be8900e09651dbad12234060362f42
Benoit Vasseur <benoit.vasr@gmail.com>
2012-06-05 14:31:27 +0200

    adds a class corresponding to the server (not in the main any more), and removed JarLoader to put them in the serveur lib

---
+++ src/main/java/com/epita/mti/plic/opensource/controlibserversample/Server.java
@@ -0,0 +1,133 @@
+package com.epita.mti.plic.opensource.controlibserversample;
+
+import com.epita.mti.plic.opensource.controlibserver.connection.ConnectionManager;
+import com.epita.mti.plic.opensource.controlibserver.jarloader.JarClassLoader;
+import com.epita.mti.plic.opensource.controlibserver.server.CLServer;
+import com.epita.mti.plic.opensource.controlibserversample.observer.JarFileObserver;
+import com.epita.mti.plic.opensource.controlibserversample.view.ServerView;
+import com.epita.mti.plic.opensource.controlibutility.serialization.ObjectReceiver;
+import com.epita.mti.plic.opensource.controlibutility.serialization.ObjectSender;
+import java.awt.AWTException;
+import java.awt.Frame;
+import java.awt.SystemTray;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Observer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Benoit "KIDdAe" Vasseur
+ */
+public class Server implements CLServer
+{
+  public final static int IN_PORT = 4200;
+  public final static int OUT_PORT = 4201;
+  private static ConnectionManager connectionManager = new ConnectionManager();
+  private static ServerView serverView = new ServerView();
+  private static Frame qrcodeView = null;
+  private JarClassLoader classLoader = new JarClassLoader(this);
+  private static ObjectReceiver receiver;
+  private static ObjectSender sender;
+
+  @Override
+  public void updatePlugins() {
+    ArrayList<Class<?>> plugins = classLoader.getPlugins();
+    for (Class<?> plugin : plugins)
+    {
+      try
+      {
+        Constructor<?> constructor = plugin.getConstructor();
+        Observer observer = (Observer) constructor.newInstance();
+        receiver.addObserver(observer);
+      }
+      catch (InstantiationException ex)
+      {
+        Logger.getLogger(ServerSample.class.getName()).log(Level.SEVERE, null, ex);
+      }
+      catch (IllegalAccessException ex)
+      {
+        Logger.getLogger(ServerSample.class.getName()).log(Level.SEVERE, null, ex);
+      }
+      catch (IllegalArgumentException ex)
+      {
+        Logger.getLogger(ServerSample.class.getName()).log(Level.SEVERE, null, ex);
+      }
+      catch (InvocationTargetException ex)
+      {
+        Logger.getLogger(ServerSample.class.getName()).log(Level.SEVERE, null, ex);
+      }
+      catch (NoSuchMethodException ex)
+      {
+        Logger.getLogger(ServerSample.class.getName()).log(Level.SEVERE, null, ex);
+      }
+      catch (SecurityException ex)
+      {
+        Logger.getLogger(ServerSample.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    }
+    classLoader.getPlugins().clear();
+  }
+
+  public void start()
+  {
+    try
+    {
+      final SystemTray tray = SystemTray.getSystemTray();
+      connectionManager.openConnection(IN_PORT, OUT_PORT);
+
+      tray.add(serverView.getTrayIcon());
+
+      while (true)
+      {
+        try
+        {
+          classLoader.initializeLoader();
+        }
+        catch (Exception ex)
+        {
+          Logger.getLogger(ServerSample.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        JarFileObserver jarFileObserver = new JarFileObserver();
+        Socket inputSocket = connectionManager.getInputSocket().accept();
+        Socket outputSocket = connectionManager.getOutputSocket().accept();
+        closeQrcodeView();
+        jarFileObserver.setClassLoader(classLoader);
+        receiver = new ObjectReceiver(inputSocket, jarFileObserver);
+        sender = new ObjectSender(outputSocket.getOutputStream());
+        new Thread(receiver).start();
+      }
+    }
+    catch (IOException ex)
+    {
+      Logger.getLogger(ServerSample.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    catch (AWTException ex)
+    {
+      Logger.getLogger(ServerSample.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+
+  public static ConnectionManager getConnectionManager()
+  {
+    return connectionManager;
+  }
+
+  public static void setQrcodeView(Frame f)
+  {
+    qrcodeView = f;
+  }
+
+  public static void closeQrcodeView()
+  {
+    if (qrcodeView != null)
+    {
+      qrcodeView.dispose();
+      qrcodeView = null;
+    }
+  }
+}
